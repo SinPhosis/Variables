da25eb205261ffdf941c73d9701380bbe97c475c
SinPhosis <sionclasher@gmail.com>
2024-12-02 17:15:30 +0800

    Variables

---
+++ V.js
@@ -0,0 +1,11 @@
+console.log("hello from JS Variables")
+const myName = 'SinPhosis'
+console.log(myName);
+const lastName = 'Void'
+console.log(lastName);
+const Martialstatus = 'none'
+console.log(Martialstatus);
+const country = 'Mongolia'
+console.log(country);
+const age = '15'
+console.log(age);
